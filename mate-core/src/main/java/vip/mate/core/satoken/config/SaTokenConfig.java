d681c45d37bd5247b508b313f3a66078b9048737
matevip <7333791@qq.com>
2023-08-24 17:53:06 +0800

    feat: 生成token采用simple-uuid的形式

--- mate-core/src/main/java/vip/mate/core/satoken/config/SaTokenConfig.java
+++ mate-core/src/main/java/vip/mate/core/satoken/config/SaTokenConfig.java
@@ -3,14 +3,11 @@ package vip.mate.core.satoken.config;
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.filter.SaServletFilter;
 import cn.dev33.satoken.interceptor.SaInterceptor;
-import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
 import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
-import cn.dev33.satoken.stp.StpLogic;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import cn.hutool.core.collection.CollectionUtil;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -124,9 +121,9 @@ public class SaTokenConfig implements WebMvcConfigurer {
                 ;
     }
 
-    @Bean
-    public StpLogic getStpLogicJwt() {
-        // Sa-Token 整合 jwt (简单模式)
-        return new StpLogicJwtForSimple();
-    }
+//    @Bean
+//    public StpLogic getStpLogicJwt() {
+//        // Sa-Token 整合 jwt (简单模式)
+//        return new StpLogicJwtForSimple();
+//    }
 }
