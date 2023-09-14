d9bf5a1fcf5248675e07d009b4cbb06d6e8ef065
matevip <7333791@qq.com>
2023-09-14 16:12:26 +0800

    docs: 删除无用的生成类

--- mate-platform/mate-system/src/main/java/vip/mate/system/service/SysRoleMenuService.java
+++ mate-platform/mate-system/src/main/java/vip/mate/system/service/SysRoleMenuService.java
@@ -2,8 +2,6 @@ package vip.mate.system.service;
 
 import vip.mate.system.entity.SysRoleMenu;
 import com.baomidou.mybatisplus.extension.service.IService;
-import vip.mate.system.vo.SysRoleMenuVO;
-import vip.mate.system.req.SysRoleMenuReq;
 import vip.mate.core.mybatis.res.PageRes;
 
 import java.util.List;
@@ -17,41 +15,6 @@ import java.util.List;
 * @since 2023-08-22
 */
 public interface SysRoleMenuService extends IService<SysRoleMenu> {
-
-    /**
-     * 列表查询
-     * @param req 参数
-     * @return 分页结果
-     */
-    PageRes<SysRoleMenuVO> queryPage(SysRoleMenuReq req);
-
-    /**
-     * 新增
-     * @param entity 实体类对象
-     * @return true：成功，false：失败
-     */
-    Boolean createData(SysRoleMenu entity);
-
-    /**
-    * 修改
-    * @param entity 实体类对象
-    * @return true：成功，false：失败
-    */
-    Boolean updateData(SysRoleMenu entity);
-
-    /**
-     * 删除
-     * @param id 主键
-     * @return true：成功，false：失败
-     */
-    Boolean removeData(Long id);
-
-    /**
-     * 单条查询
-     * @param id 主键
-     * @return true：成功，false：失败
-     */
-    SysRoleMenuVO getData(Long id);
 
     /**
      * 保存角色菜单
--- mate-platform/mate-system/src/main/java/vip/mate/system/service/impl/SysRoleMenuServiceImpl.java
+++ mate-platform/mate-system/src/main/java/vip/mate/system/service/impl/SysRoleMenuServiceImpl.java
@@ -4,16 +4,10 @@ import vip.mate.system.entity.SysRoleMenu;
 import vip.mate.system.mapper.SysRoleMenuMapper;
 import vip.mate.system.service.SysRoleMenuService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import vip.mate.system.vo.SysRoleMenuVO;
-import vip.mate.system.req.SysRoleMenuReq;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.springframework.stereotype.Service;
 import java.util.*;
 import cn.hutool.core.collection.CollectionUtil;
-import vip.mate.system.convert.SysRoleMenuConvert;
-import vip.mate.core.mybatis.res.PageRes;
-
 
 /**
  * <p>
@@ -25,39 +19,6 @@ import vip.mate.core.mybatis.res.PageRes;
  */
 @Service
 public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuMapper, SysRoleMenu> implements SysRoleMenuService {
-
-    @Override
-    public PageRes<SysRoleMenuVO> queryPage(SysRoleMenuReq req) {
-        SysRoleMenu info = SysRoleMenuConvert.INSTANCE.convert(req);
-        Page<SysRoleMenu> pageData = baseMapper.selectPage(new Page<>(req.getPageNo(), req.getPageSize()), Wrappers.query(info));
-        if(CollectionUtil.isEmpty(pageData.getRecords())){
-            return PageRes.empty();
-        }
-        List<SysRoleMenuVO> vos = SysRoleMenuConvert.INSTANCE.toVo(pageData.getRecords());
-        return new PageRes<>(vos, pageData.getTotal(),req);
-    }
-
-    @Override
-    public Boolean createData(SysRoleMenu entity) {
-        return baseMapper.insert(entity) > 0;
-    }
-
-    @Override
-    public Boolean updateData(SysRoleMenu entity) {
-        return baseMapper.updateById(entity) > 0;
-    }
-
-    @Override
-    public Boolean removeData(Long id) {
-        return baseMapper.deleteById(id) > 0;
-    }
-
-
-    @Override
-    public SysRoleMenuVO getData(Long id) {
-        final SysRoleMenu info = baseMapper.selectById(id);
-        return SysRoleMenuConvert.INSTANCE.convertVo(info);
-    }
 
     @Override
     public Boolean saveRoleMenu(Long id, List<Long> menuIds) {
