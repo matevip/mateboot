04b0d2423ccde2791c75e02abceb328a2aaabe54
matevip <7333791@qq.com>
2023-09-21 11:31:04 +0800

    feat: 使用setup方式完善账户信息页面

--- mate-ui-admin/src/views/userCenter/index.vue
+++ mate-ui-admin/src/views/userCenter/index.vue
@@ -4,9 +4,9 @@
 			<el-container>
 				<el-header style="height: auto;display: block;">
 					<div class="user-info-top">
-						<el-avatar :size="70" src="img/avatar.jpg"></el-avatar>
-						<h2>{{ user.userName }}</h2>
-						<p><el-tag effect="dark" round size="large">{{ user.role }}</el-tag></p>
+						<el-avatar :size="70" :src="user.avatar"></el-avatar>
+						<h2>{{ user.username }}</h2>
+						<p><el-tag effect="dark" round size="large">{{ user.realName }}</el-tag></p>
 					</div>
 				</el-header>
 				<el-main class="nopadding">
@@ -30,7 +30,7 @@
 			<Suspense>
 				<template #default>
 					<div>
-						<component :is="page" />
+						<component :is="comArr[page]" />
 					</div>
 
 				</template>
@@ -42,7 +42,99 @@
 	</el-container>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref, reactive, onMounted, shallowRef, defineAsyncComponent } from 'vue'
+import store from '@/store'
+
+const account = defineAsyncComponent(() => import('./user/account.vue'))
+const seting = defineAsyncComponent(() => import('./user/seting.vue'))
+const pushSettings = defineAsyncComponent(() => import('./user/pushSettings.vue'))
+const password = defineAsyncComponent(() => import('./user/password.vue'))
+const space = defineAsyncComponent(() => import('./user/space.vue'))
+const logs = defineAsyncComponent(() => import('./user/logs.vue'))
+const upToEnterprise = defineAsyncComponent(() => import('./user/upToEnterprise.vue'))
+
+const comArr: any = shallowRef({
+	"account": account,
+	"seting": seting,
+	"pushSettings": pushSettings,
+	"password": password,
+	"space": space,
+	"logs": logs,
+	"upToEnterprise": upToEnterprise
+})
+
+const menu = reactive([{
+	groupName: "基本设置",
+	list: [
+		{
+			icon: "el-icon-postcard",
+			title: "账号信息",
+			component: "account"
+		},
+		{
+			icon: "el-icon-operation",
+			title: "个人设置",
+			component: "seting"
+		},
+		{
+			icon: "el-icon-lock",
+			title: "密码",
+			component: "password"
+		},
+		{
+			icon: "el-icon-bell",
+			title: "通知设置",
+			component: "pushSettings"
+		}
+	]
+},
+{
+	groupName: "数据管理",
+	list: [
+		{
+			icon: "el-icon-coin",
+			title: "存储空间信息",
+			component: "space"
+		},
+		{
+			icon: "el-icon-clock",
+			title: "操作日志",
+			component: "logs"
+		}
+	]
+},
+{
+	groupName: "账号升级",
+	list: [
+		{
+			icon: "el-icon-office-building",
+			title: "升级为企业账号",
+			component: "upToEnterprise"
+		}
+	]
+}])
+
+const user = reactive({
+	username: "",
+	realName: "",
+	avatar: ""
+})
+const page = ref("account")
+
+const openPage = (item: any) => {
+	page.value = item.index
+}
+
+onMounted(() => {
+	user.username = store.userStore.user.username
+	user.realName = store.userStore.user.realName
+	user.avatar = store.userStore.user.avatar
+})
+
+
+</script>
+<!-- <script>
 import { defineAsyncComponent } from 'vue'
 import store from '@/store'
 
@@ -138,4 +230,4 @@ export default {
 		}
 	}
 }
-</script>
+</script> -->
