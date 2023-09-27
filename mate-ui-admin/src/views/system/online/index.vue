b04c0803fa51aaa4bc24b9659f19fd38be7280ec
david <david@daotiandi.cn>
2023-09-27 08:31:45 +0800

    style: 调整样式

--- mate-ui-admin/src/views/system/online/index.vue
+++ mate-ui-admin/src/views/system/online/index.vue
@@ -4,6 +4,7 @@
 			<div class="left-panel">
 				<div class="right-panel-search">
 					<el-input v-model="search.name" placeholder="姓名" clearable></el-input>
+					<el-date-picker v-model="search.date" type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期"></el-date-picker>
 					<el-button type="primary" icon="el-icon-search" @click="upsearch"></el-button>
 				</div>
 			</div>
@@ -45,7 +46,8 @@ import MTable from '@/components/MTable/index.vue'
 import store from '@/store'
 
 const search = reactive({
-	name: null
+	name: null,
+	date: null,
 })
 const apiObj = ref(useOnlineUserPage)
 const selection = ref([])
@@ -55,7 +57,6 @@ const token = store.userStore.token
 
 //删除
 const tableDel = async (row: any) => {
-	var reqData = { id: row.token }
 	var res: any = await useKickOut(row.token);
 	if (res.code == 0) {
 		tableRef.value.refresh()
